7ae53176b9e6d35bf4728b12a3037390443848eb
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-02 09:09:23 +0200

    cleaner

--- prolog.lua
+++ prolog.lua
@@ -5,10 +5,7 @@ swipl
 local pkgName = "swipl prolog"
 local version = "???"
 local base    = pathJoin(os.getenv("HOME")) 
--- local base    = pathJoin(os.getenv("HOME"), version)
-prepend_path("PATH", pathJoin(base,"bin"))  -- /app/valgrind/3.7.0/bin
--- setenv(      "SITE_VALGRIND_INC", pathJoin(base,"include"))
--- setenv(      "SITE_VALGRIND_LIB", pathJoin(base,"lib"))
+prepend_path("PATH", pathJoin(base,"bin"))  
 
 whatis("Name: ".. pkgName)
 whatis("Version: " .. version)
--- rust.lua
+++ rust.lua
@@ -1,14 +1,11 @@
 help([[
-swipl
+rust in cargo
 ]])
 
 local pkgName = "rust"
 local version = "???"
 local base    = pathJoin(os.getenv("HOME"), ".cargo") 
--- local base    = pathJoin(os.getenv("HOME"), version)
-prepend_path("PATH", pathJoin(base,"bin"))  -- /app/valgrind/3.7.0/bin
--- setenv(      "SITE_VALGRIND_INC", pathJoin(base,"include"))
--- setenv(      "SITE_VALGRIND_LIB", pathJoin(base,"lib"))
+prepend_path("PATH", pathJoin(base,"bin"))  
 
 whatis("Name: ".. pkgName)
 whatis("Version: " .. version)
